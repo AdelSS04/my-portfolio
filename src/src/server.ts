2bb93860384e103d8edaa7254ada3e1fa932bb21
Adel Lajil <ajiladel@gmail.com>
2025-09-18 11:10:40 -0400

    refactor: update import path in API entry point and export app instance from server

--- src/src/server.ts
+++ src/src/server.ts
@@ -9,9 +9,8 @@ import { join } from 'node:path';
 
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
-const app = express();
+export const app = express();
 const angularApp = new AngularNodeAppEngine();
-
 /**
  * Example Express Rest API endpoints can be defined here.
  * Uncomment and define endpoints as necessary.
@@ -65,3 +64,4 @@ if (isMainModule(import.meta.url)) {
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
  */
 export const reqHandler = createNodeRequestHandler(app);
+
