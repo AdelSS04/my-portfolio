115a5fb9cc2ec71d990696d45422d05bd666182b
Adel Lajil <ajiladel@gmail.com>
2025-09-20 02:48:39 -0400

    refactor: update docker-compose and renewal script for improved certificate management

--- renew-certs.sh
+++ renew-certs.sh
@@ -23,15 +23,10 @@ sudo docker stop nginx-proxy
 # Génère ou renouvelle le certificat dans le dossier du projet
 sudo certbot certonly \
   --standalone \
-  --preferred-challenges http \
-  --agree-tos \
   --non-interactive \
-  --email "$EMAIL" \
-  -d "$DOMAIN" \
-  --config-dir "$CERT_PATH"/config \
-  --work-dir "$CERT_PATH"/work \
-  --logs-dir "$CERT_PATH"/logs
-
+  --force-renewal \
+  -d "$DOMAIN"
+  
 # Start nginx-proxy again
 sudo docker start nginx-proxy
 
