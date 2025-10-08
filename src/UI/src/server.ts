05627cbc4acb7e4e9b096b4198a4b6792f18a6f4
Adel Lajil <ajiladel@gmail.com>
2025-10-08 16:39:03 -0400

    fix

--- src/UI/src/server.ts
+++ src/UI/src/server.ts
@@ -5,11 +5,13 @@ const angularApp = new AngularAppEngine();
 /**
  * This is a request handler used by the Angular CLI (dev-server and during build).
  */
-export const reqHandler = createRequestHandler(async (req) => {
+export const reqHandler = createRequestHandler(async (req: Request) => {
 	const res = await angularApp.handle(req);
 
 	return res ?? new Response('Page not found.', { status: 404 });
 });
 
-
-export default { fetch: reqHandler };
+// Export for Cloudflare Workers
+export default { 
+	fetch: reqHandler
+};
